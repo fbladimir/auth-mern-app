0d7d8651550cb15b49a130cb9ce3599049d948d2
fbladimir <franklinbladimirck@yahoo.com>
2023-03-04 14:28:05 -0500

    login ui is completed with assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+/* Import All Components */ 
+import UserName from './components/Username'; 
+import Password from './components/Password'; 
+import Register from './components/Register'; 
+import Profile from './components/Profile'; 
+import Recovery from './components/Recovery'; 
+import Reset from './components/Reset'; 
+import PageNotFound from './components/PageNotFound'; 
+import Username from './components/Username';
+
 
 /* Root Routes */ 
 
 const router = createBrowserRouter([ 
     { 
         path: '/', 
-        element : <div> Root Route </div>
+        element : <Username> </Username>
     }, 
     { 
-        path: '/register', 
-        element : <div> Register Route </div>
+        path: '/Register', 
+        element : <Register> </Register>
+    },
+    { 
+        path: '/Password', 
+        element : <Password> </Password>
+    },
+    { 
+        path: '/Profile', 
+        element : <Profile> </Profile>
+    },
+    { 
+        path: '/Recovery', 
+        element : <Recovery> </Recovery>
+    },
+    { 
+        path: '/Reset', 
+        element : <Reset> </Reset>
+    },
+    { 
+        path: '/*', 
+        element : <PageNotFound> </PageNotFound>
     },
 ])
 
@@ -19,7 +49,7 @@ export default function App() {
     <main>
 
         <RouterProvider router={router}> </RouterProvider>
-        
+
 
    </main>
   )
