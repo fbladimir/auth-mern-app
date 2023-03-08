06116f2e62f87944b755b71e9a7cb426fc5dd18a
fbladimir <franklinbladimirck@yahoo.com>
2023-03-08 00:07:05 -0500

    recovery component ui has been started

--- client/src/components/Recovery.js
+++ client/src/components/Recovery.js
@@ -10,7 +10,7 @@ import styles from '../styles/Username.module.css';
 
 
 
-export default function Password() {
+export default function Recovery() {
 
   const formik = useFormik({ 
     initialValues : { 
@@ -33,24 +33,30 @@ export default function Password() {
         <div className={styles.glass}> 
 
           <div className="title flex flex-col items-center"> 
-            <h4 className='text-5xl font-bold'> Hello </h4> 
+            <h4 className='text-5xl font-bold'> Recovery </h4> 
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'> 
-              Learning how to code with FrankyDanky 
+             Enter OTP to recover password
             </span>
           </div> 
 
-          <form className='py-1' onSubmit={formik.handleSubmit}>
-            <div className='profile flex justify-center py-4'> 
-              <img src={avatar} className={styles.profile_img} alt="avatar" />   
-            </div> 
+          <form className='pt-20' onSubmit={formik.handleSubmit}>
+
+
+           
 
             <div className="textbox flex flex-col items-center gap-6">
-              <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='Password' /> 
-              <button className={styles.btn} type='submit'> Sign Up </button>
+
+              <div className='input text-center'> 
+
+              <span className='py-4 text-sm text-left text-gray-500'> Please enter 6 digit OTP sent to your email address </span> 
+              <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='OTP' /> 
+
+              </div>
+              <button className={styles.btn} type='submit'> Recover</button>
             </div>
 
             <div className="text-center py-4"> 
-              <span className='text-gray-500'> Forgot Password? <Link className='text-red-500' to="/Recovery"> Recover Now</Link></span>
+              <span className='text-gray-500'> Can't get OTP? <button className='text-red-500'> Recover Now </button></span>
 
             </div>  
 
