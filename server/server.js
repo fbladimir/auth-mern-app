dcc77c949834aad76173463b2b4bcdcaa3242db0
fbladimir <franklinbladimirck@yahoo.com>
2023-03-08 22:03:08 -0500

    setting up backend server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from 'express'; 
+import cors from 'cors'; 
+import morgan from 'morgan'; 
+
+
+const app = express(); 
+
+/* Middlewares */ 
+
+app.use(express.json()); 
+
+app.use(cors()); 
+app.use(morgan('tiny')); 
+
+app.disable('x-powered-by'); //less hackers know about our stack 
+
+
+const port = 8080; 
+
+/* HTTTP Get Request */ 
+
+
+app.get('/', (req, res) => { 
+    res.status(201).json("Home GET Request"); 
+
+}); 
+
+/* Start server */ 
+
+app.listen(port, () => { 
+    console.log(`Server connected to http://localhost:${port}`); 
+
+})
+
+
+
