3619ce20b252e449a8ab4c0a0fd2bf26f5a73cf7
fbladimir <franklinbladimirck@yahoo.com>
2023-03-09 00:02:56 -0500

    routes to controllers created

--- server/router/route.js
+++ server/router/route.js
@@ -2,26 +2,30 @@ import { Router } from 'express';
 
 const router = Router(); 
 
+/*Import all controllers*/ 
+
+import * as controller from '../controllers/appController.js'; 
+
 /* Post Method */ 
 
-router.route('/register').post((req, res) => res.json('register route')); 
-router.route('/registerMail').post(); //send email 
-router.route('/authenticate').post(); // authenticate user 
-router.route('/login').post(); // login in app 
+router.route('/register').post(controller.register); 
+//router.route('/registerMail').post(); //send email 
+router.route('/authenticate').post((req, res) => res.end()); // authenticate user 
+router.route('/login').post(controller.login); // login in app 
 
 
 
 /*GET methods */ 
 
-router.route('/user/:username').get();  //user with username 
-router.route('/generateOTP').get();  // generate random otp 
-router.route('/verifyOTP').get();  // verify generated otp 
-router.route('/createResetSession').get();  //reset all the variables 
+router.route('/user/:username').get(controller.getUser);  //user with username 
+router.route('/generateOTP').get(controller.generateOTP);  // generate random otp 
+router.route('/verifyOTP').get(controller.verifyOTP);  // verify generated otp 
+router.route('/createResetSession').get(controller.createResetSession);  //reset all the variables 
 
 
 /* PUT METHODS */ 
-router.route('/updateuser').put();  //update the user profile 
-router.route('/resetPassword').put();  //use to reset password 
+router.route('/updateuser').put(controller.updateUser);  //update the user profile 
+router.route('/resetPassword').put(controller.resetPassword);  //use to reset password 
 
 export default router; 
 
